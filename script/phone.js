f68921b3c3a9115a0455f726147ad41083dd969a
Plabon Saha <sahaplabon66@gmail.com>
2023-08-30 14:57:49 +0600

    hshs

--- script/phone.js
+++ script/phone.js
@@ -19,14 +19,14 @@ const displayphones = (phones,isShowAll) => {
     else{
         showAllContainer.classList.add('hidden')
     }
-    console.log('is show all', isShowAll);
+    // console.log('is show all', isShowAll);
     // display only first 10 phone if not show all
     if(!isShowAll){
         phones = phones.slice(0,10)
     }
     
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card bg-gray-200 p-4 shadow-xl`
@@ -35,8 +35,9 @@ const displayphones = (phones,isShowAll) => {
         <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+          <div class="card-actions justify-center">
+            <button onclick = "handleShowDetails('${phone.slug}') 
+            "class="btn btn-primary">Show Details</button>
             </div>
         </div>
         `;
@@ -46,6 +47,40 @@ const displayphones = (phones,isShowAll) => {
     // hide loading spinner
     toggleLoadingSpinner(false)
 }
+
+//
+const handleShowDetails = async (id) => {
+    // console.log('clicked show details', id)
+    // load single phone data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+
+    showPhoneDetails(phone)
+
+}
+const showPhoneDetails = (phone) => {
+    console.log(phone);
+    const phoneName = document.getElementById('show-detail-phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById('show-detail-container');
+
+    showDetailContainer.innerHTML= `
+        <img src="${phone.image}"alt="" />
+        <p><span>Storage:</span>${phone?.mainFeatures?.storage}</p>
+        <p><span>GPS:</span>${phone.others?.GPS || 'No GPS available'}</p>
+        <p><span>GPS:</span>${phone.others?.GPS ? phone.others.GPS : 'No GPS available in this device'}</p>
+    `
+
+    // show the modal
+    show_details_modal.showModal();
+}
+
+
+
+
+
 // handle search button
 const handleSearch =(isShowAll) =>{
     toggleLoadingSpinner(true)
